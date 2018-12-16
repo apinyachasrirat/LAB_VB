8096aaaad21936ed212b0cd2c78b7414546067e2
apinyachasrirat <apinya270540@gmail.com>
2018-12-16 22:14:44 +0700

    16-12-61

---
+++ lab1/Form2.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim vista, frista, sum As Decimal
+        vista = Val(TextBox1.Text) * 5 / 100
+        frista = Val(TextBox2.Text) * 10 / 100
+        sum = Val(TextBox1.Text) + Val(TextBox2.Text)
+        Label5.Text = sum
+        Label6.Text = vista + frista
+    End Sub
+End Class
