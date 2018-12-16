bf0727e3f88b88bb6e9691bbea151578bd203122
apinyachasrirat <apinya270540@gmail.com>
2018-12-16 22:00:24 +0700

    first commit

---
+++ lab1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class lab1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Label4.Text = Val(TextBox2.Text) * 12
+    End Sub
+End Class
